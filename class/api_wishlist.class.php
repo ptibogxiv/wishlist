d0336b663df19a99cda3c8422e47394503b3ca32
ptibogxiv <support@ptibogxiv.net>
2024-08-07 16:27:53 +0200

    Update api_wishlist.class.php

--- class/api_wishlist.class.php
+++ class/api_wishlist.class.php
@@ -120,7 +120,7 @@ class Wishlist extends DolibarrApi
 	 * @param int		$page		        Page number
 	 * @param string   	$thirdparty_ids	Thirdparty ids to filter wishlists of.
 	 * @param string    $sqlfilters         Other criteria to filter answers separated by a comma. Syntax example "(t.ref:like:'SO-%') and (t.datec:<:'20160101')" 
-     * @param bool             $pagination_data     If this parameter is set to true the response will include pagination data. Default value is false. Page starts from 0*
+     * @param bool      $pagination_data     If this parameter is set to true the response will include pagination data. Default value is false. Page starts from 0*
 	 * @return array                Array of session objects
 	 * 
 	 *@throws RestException
